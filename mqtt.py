d60139529385faf61442ef3da2b1f550fd9a77c1
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2020-04-25 01:25:36 +0200

    Exit if ddisconnected from mqtt broker

--- mqtt.py
+++ mqtt.py
@@ -1,5 +1,7 @@
 import json
 import os
+import sys
+
 import paho.mqtt.client as mqtt
 
 
@@ -11,6 +13,15 @@ client = mqtt.Client()
 client.username_pw_set(username=os.environ['MQTT_USERNAME'].strip(),
                        password=os.environ['MQTT_PASSWORD'].strip())
 client.connect(broker_url, broker_port, keepalive=600)
+
+def on_disconnect(_client, _userdata, rc):
+    if rc != 0:
+        print("Unexpected disconnection. Exiting...")
+        sys.exit(1)
+    else:
+        print("Disconnected successfully")
+
+client.on_disconnect = on_disconnect
 
 
 def send(ts, measurement):
